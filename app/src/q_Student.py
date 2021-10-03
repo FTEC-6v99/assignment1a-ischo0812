d4627f834bb9ddd5b3a5c3d432a2aeda803aa5d6
ischo0812 <leesoo.cho@utdallas.edu>
2021-10-03 12:38:56 -0500

    First two assignments : q_Person and q_Student

---
+++ app/src/q_Student.py
@@ -0,0 +1,26 @@
+# Create a class named Student
+# The Student class must extend the Person class
+# In addition to the Person class attribute, the Student class must have the following attributes:
+#   1. id (int)
+#   2. grade (float)
+
+from q_Person import Person
+
+
+class Student(Person):
+    def __init__(self, name: str, age: int, id: int, grade: float) -> None:
+        Person.__init__(self, name, age)
+        self.id = id
+        self.grade = grade
+
+        print("ID : {2}, Name : {0}, Age : {1}, Grade : {3}".format(
+            self.name, self.age, self.id, self.grade))
+
+
+p1 = Person(name='Abraham', age=31)
+p2 = Person(name='William', age=35)
+p3 = Person('Johnson', 25)
+
+p4 = Student(name='Tommy', age=31, id=202019, grade=97)
+p5 = Student(name='Michael', age=35, id=202025, grade=93)
+p6 = Student('Adam', 36, 202066, 90)
---
+++ app/src/stats.py
@@ -0,0 +1,5 @@
+class stats():
+    def __init__(self, points: int, assists: int, fouls: int):
+        self.points = float(points)
+        self.assists = float(assists)
+        self.fouls = float(fouls)
